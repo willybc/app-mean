2accbd330d6fe5c8fe7ac9883de037e6aa6e81d4
willybc <willybc06@gmail.com>
2018-09-25 01:38:21 -0300

    model question

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from './question.model';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
 
+export class QuestionComponent implements OnInit {
+  //Model
+  question: Question = new Question(
+    'Esta es una nueva pregunta sobre Android',
+    'Miren, tengo una duda con una aplicacion',
+    new Date,
+    'devicon-android-plain'
+  );
+  
   constructor() { }
 
   ngOnInit() {
